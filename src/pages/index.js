a0622bfaed336a6778e4f3fefdb908189079fb19
Darrk Moberg <30670444+mdarrik@users.noreply.github.com>
2019-08-01 00:28:29 -0700

    Fix: this will work!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
-export default basePage = () => (
+export default () => (
   <>
     <h1>This is a demo for @mdarrik/gatsby-theme-soundboard</h1>
     <Link to="/sounds">Click Here to See the Theme in Action</Link>
