7a2971ebabe79b2bb7bdc5a6083044c07185e221
Darrk Moberg <30670444+mdarrik@users.noreply.github.com>
2019-08-01 00:26:17 -0700

    fix: removes random parentheses

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'gatsby';
-export default (basePage = () => (
+export default basePage = () => (
   <>
     <h1>This is a demo for @mdarrik/gatsby-theme-soundboard</h1>
     <Link to="/sounds">Click Here to See the Theme in Action</Link>
   </>
-));
+);
